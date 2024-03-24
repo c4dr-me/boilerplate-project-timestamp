6a9fa592522c8766c0abe28fd2aa810f1c95f12f
c4dr me <nitrohex69@Gmail.com>
2024-03-24 16:09:51 +0000

    commit

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
+const { is } = require('express/lib/request');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -18,11 +19,31 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+const invalidDate = (date) => date.toUTCString() === 'Invalid Date';
+
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date", function(req, res) {
+  let date = new Date(req.params.date);
+  if (invalidDate(date)) {
+    date = new Date(parseInt(req.params.date));
+  }
+  if (invalidDate(date)) { 
+    res.json({ error: "Invalid Date" }); 
+    return;
+  }
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
 });
+
+app.get("/api", (req,res)=>{
+  res.json({
+    unix: new Date().getTime(),
+    utx: new Date().toUTCString()
+  })
+})
 
 
 
